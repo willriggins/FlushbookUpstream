39cc7e2bd28b6c2c80d9a68b98e1597b6425f058
jeffryporter <jporter7@gmail.com>
2016-06-16 16:45:46 -0400

    Add update and delete methods, put and delete routes.

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,12 +1,11 @@
 package com.theironyard;
 
+import jdk.nashorn.internal.parser.JSONParser;
+import jodd.json.JsonParser;
 import org.h2.tools.Server;
 import spark.Spark;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -15,9 +14,24 @@ public class Main {
         stmt.execute("CREATE TABLE IF NOT EXISTS toilets (id IDENTITY, facility VARCHAR, latitude DOUBLE, longitude DOUBLE, access INT, capacity INT, cleanliness INT)");
     }
 
-//    public static ArrayList<Toilet> selectToilets(Connection conn) {
-//
-//    }
+    public static void updateToilet(Connection conn, Toilet toilet) throws SQLException
+    {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE toilets SET facility=?, latitude=?, longitude=?, access=?, capacity=?, cleanliness=? WHERE id = ?");
+        stmt.setString(1,toilet.facility);
+        stmt.setDouble(2,toilet.lat);
+        stmt.setDouble(3,toilet.lon);
+        stmt.setInt(4,toilet.access);
+        stmt.setInt(5,toilet.capacity);
+        stmt.setInt(6,toilet.cleanliness);
+        stmt.setInt(7,toilet.id);
+    }
+
+    public static void deleteToilet(Connection conn, int id) throws SQLException
+    {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM toilets WHERE id = ?");
+        stmt.setInt(1,id);
+        stmt.execute();
+    }
 
     public static void main(String[] args) throws SQLException {
         Server.createWebServer().start();
@@ -27,36 +41,38 @@ public class Main {
         Spark.externalStaticFileLocation("public");
         Spark.init();
 
-//        Spark.get(
-//                "/flush",
-//                (request, response) -> {
-//                    //ArrayList<Toilet> toilets =
-//
-//                }
-//        );
-//        Spark.post(
-//                "/flush",
-//                (request, response) -> {
-//
-//                }
-//        );
-//        Spark.put(
-//                "/flush",
-//                (request, response) -> {
-//
-//                }
-//        );
-//        Spark.delete(
-//                "/flush",
-//                (request, response) -> {
-//
-//                }
-//        );
+        Spark.get(
+                "/flush",
+                (request, response) -> {
+                    //ArrayList<Toilet> toilets =
 
+                }
+        );
+        Spark.post(
+                "/flush",
+                (request, response) -> {
 
-
-
-
-
+                }
+        );
+        Spark.put(
+                "/flush",
+                (request, response) ->
+                {
+                    String body = request.body();
+                    JsonParser p = new JsonParser();
+                    Toilet toilet = p.parse(body,Toilet.class);
+                    updateToilet(conn, toilet);
+                    return"";
+                }
+        );
+        Spark.delete(
+                "/flush/:id",
+                (request, response) ->
+                {
+                    int id = Integer.valueOf(request.params(":id"));
+                    deleteToilet(conn, id);
+                    return"";
+                }
+        );
     }
 }
