94c5801d98db229a39a274e1ee6be9ee29d50632
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-06-17 11:39:20 -0400

    doin stuff

--- public/main.js
+++ public/main.js
@@ -38,7 +38,7 @@ var mainPage = {
     //crud ajax functions
     create(){
         $.ajax({
-            url:"url",
+            url:"localhost4567",
             method: "POST",
             data: restroom,
             success(data) {
@@ -53,7 +53,7 @@ var mainPage = {
 
     read() {
         $.ajax({
-            url:"url",
+            url:"localhost4567",
             method: "GET",
 
             success(data) {
@@ -78,7 +78,7 @@ var mainPage = {
 
     update(){
         $.ajax({
-            url:"",
+            url:`localhost4567`,
             method: "PUT",
 
             success(data) {
@@ -93,7 +93,7 @@ var mainPage = {
 
     destroy(deleteId) {
         $.ajax({
-            url:"",
+            url:"localhost4567",
             method: "DELETE",
 
             success(data) {
