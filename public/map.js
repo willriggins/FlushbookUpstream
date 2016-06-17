321ce868c0eec200e874171cef36c2df1c8556ff
phillipdanielscott <phillipd20@yahoo.com>
2016-06-17 15:49:16 -0400

    changed background to autoplay

--- public/map.js
+++ public/map.js
@@ -4,7 +4,7 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.7765, lng: -79.9311},
-    zoom: 2
+    zoom: 15
   });
   var marker = new google.maps.Marker({
     position: myLatLng,
